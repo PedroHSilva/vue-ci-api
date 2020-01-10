f06fa997aefbc54a481029574fdb3c5f770acae8
Pedro H Silva <pedro_silva@id.uff.br>
2020-01-09 23:33:57 -0300

    implementação do crud com codeigniter

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,83 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+  <div class="container">
+    <h1 class="text-center mt-5">My app</h1>
+
+    <div class="d-flex justify-content-center mt-5 border border-dark p-5 shadow">
+      <button @click="get" class="btn btn-primary">GET</button>
+      <button @click="save" class="btn btn-success">SAVE</button>
+      <button @click="update" class="btn btn-warning">UPDATE</button>
+      <button @click="remove" class="btn btn-danger">DELETE</button>
+    </div>
+
+    <ul class="list-group">
+      <li class="list-group-item" v-for="course in courses">
+        <strong>  id: {{ course.id }} </strong>
+        <p> {{ course.title }} </p>
+        <strong class="text-danger"> {{ course.couponCode }} </strong>
+        <strong class="text-info"> {{ course.price }} </strong>
+      </li>
     </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  import axios from "axios"
+  export default {
+
+    data() {
+      return {
+        courses : []
+      } 
+    },
+
+    methods : {
+      get() {
+        axios.get("http://thalinutri.com.br/ci/vue-ci/api/get_all_data")
+          .then(response=> {
+            this.courses = response.data
+          })
+        
+      },
+      save() {
+        let course = {
+          title: "API",
+          couponCode: "APITESTEVUE",
+          price: 22.28
+        }
+
+        axios.post("http://thalinutri.com.br/ci/vue-ci/api/save", JSON.stringify(course))
+          .then(response=>{
+            console.log(response);
+          })
+
+      },
+      update() {
+        let course = {
+          title: "UPDATE34",
+          couponCode: "APITESTEVUE",
+          price: 22.28,
+          id: 1
+        }
+
+        axios.post("http://thalinutri.com.br/ci/vue-ci/api/update", JSON.stringify(course))
+          .then(response=>{
+            console.log(response);
+          })
+      },
+      remove() {
+        let course = {
+          id: 8
+        }
+        axios.post("http://thalinutri.com.br/ci/vue-ci/api/delete", JSON.stringify(course))
+        .then(response=> {
+          console.log(response);
+        })
+      }
     }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
